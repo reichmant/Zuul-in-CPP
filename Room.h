78e1bd866131bea042758a836a6aa969908d9e14
Thomas Reichman <reichmant@xavier.edu>
2015-10-05 10:44:59 -0400

    Transferred Project to GitHub
    Command and CommandWords finished, most of Parser/Game/Room too.

---
+++ Room.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+//#include <iostream>
+//#include <string>
+#include <vector>
+#include <map>
+#include <set>
+
+//#include "Parser.h"
+using namespace std;
+
+#ifndef Room_H
+#define Room_H
+class Room
+{
+        private:
+                string description;
+                map<string,Room> exits;
+        public:
+                Room(string desc);
+                void setExit(string direction, Room neighbor);
+                string getShortDescription();
+                string getLongDescription();
+                string getExitString();
+                Room *getExit(string direc);
+
+};
+
+#endif
